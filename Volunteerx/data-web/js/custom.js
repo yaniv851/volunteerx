4addf26d0514b682e381f6da4859a67ae7421f2c
Mariel <borodkinmariel@gmail.com>
2021-05-23 19:19:02 +0300

    lesson wokrk

--- Volunteerx/data-web/js/custom.js
+++ Volunteerx/data-web/js/custom.js
@@ -368,5 +368,18 @@ $(function () {
 		interval: 5000
 	});
 
-
+	$('.nav-toggle').click(function () {
+		var collapse_content_selector = $(this).attr('href');
+		var toggle_switch = $(this);
+		$(collapse_content_selector).toggle(function () {
+			if ($(this).css('display') == 'none') {
+				toggle_switch.html('Read More');
+			} else {
+				toggle_switch.html('Read Less');
+			}
+		});
+	});
+	
 });
+
+
